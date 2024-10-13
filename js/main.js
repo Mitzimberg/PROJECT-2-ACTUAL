c0a60286b33031ec80f57ff6104bcddb5a35dca8
Mitzimberg <BlakeMitzimberg003@gmail.com>
2024-10-13 18:32:05 -0500

    Work

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-// JS scripts placed here
+const images = document.querySelectorAll('.hero-images img');
+let currentIndex = 0;
+
+function cycleImages() {
+    images[currentIndex].classList.remove('active');
+    currentIndex = (currentIndex + 1) % images.length;
+    images[currentIndex].classList.add('active');
+}
+
+setInterval(cycleImages, 6000); // Change image every 3 seconds
+
+function showTab(tabId) {
+    // I've got no real idea what any of this means, all of this was jacked
+    const tabItems = document.querySelectorAll('.tab-item');
+    tabItems.forEach(item => {
+        item.classList.remove('active');
+    });
+
+    // Remove active class from all tabs
+    const tabs = document.querySelectorAll('.tab');
+    tabs.forEach(tab => {
+        tab.classList.remove('active');
+    });
+
+    document.getElementById(tabId).classList.add('active');
+    document.querySelector(`.tab[onclick*="${tabId}"]`).classList.add('active');
+}
+
+showTab('tab1');
